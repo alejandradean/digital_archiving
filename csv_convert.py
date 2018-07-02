995d80abe83235dc4370b4e3eb3fd7db61b9e104
Alejandra Dean <alejandra.dean@gmail.com>
2018-07-01 21:48:05 -0400

    first round of comments

--- csv_convert.py
+++ csv_convert.py
@@ -1,12 +1,30 @@
+# the csv module contains a library of code specifically written to handle data in .csvs
 import csv
-f = open('vol_1_test.csv')
-csv_f = csv.reader(f)
-data = []
 
-for row in csv_f:
-    data.append(row)
+# the open() function, a built-in Python function, opens my .csv file, assigning it to variable "f"
+# note: the path is relative, meaning the .csv is in the same directory as this script  
+f = open('vol_1_test.csv')
+
+# csv.reader() was imported above and parses an opened file, assigning it to variable "csv_f"
+csv_f = csv.reader(f)
+
+# the parsed data from csv.reader() is converted into a list, and assigned to variable "all_rows"
+# we do this because csv.reader() returns something called a generator
+# generators have to be used in certain ways, so it's much easier to turn this into a list
+all_rows = list(csv_f)
+
+# the header row is separated out from the rest of the data; positionality in Python begins with 0
+header_row = all_rows[0]
+data_rows = all_rows[1:]
+
+# closes .csv file
 f.close()
 
+# defines our custom function that converts the iterated data in our new list into XML
+# %s is a placeholder for the data in each column
+# this script could be improved by not hard-coding the fields; for example, if I use this on a .csv with different fields it would not work
+# for simplicity I left it as is to demonstrate the basics of this function
+# if you're looking for a challenge try to convert this to something that could work with any .csv file
 def convert_row(row):
     return """<vitalRecord>
     <filename>%s</filename>
@@ -23,9 +41,22 @@ def convert_row(row):
     <vitalsPage>%s</vitalsPage>
 </vitalRecord>""" % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11])
 
-new_xml_file = open('vol_1_test.xml', 'w')
-new_xml_file.write('\n'.join([convert_row(row) for row in data[1:]]))
+# opens a new .xml file that we are writing to, indicated by 'w'
+new_xml_file = open('converted.xml', 'w')
 
+# we loop over each row, not including the header
+for row in data_rows:
+    
+    # convert each row to XML using our function above
+    xml_record = convert_row(row)
+    
+    # write that converted XML to our new file
+    new_xml_file.write(xml_record)
+    
+    # add a new line between records
+    new_xml_file.write('\n')
+
+# close our file
 new_xml_file.close()
     
     
