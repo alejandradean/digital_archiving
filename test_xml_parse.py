7b441aaf8a082a9550c6f094cbaabd665a2e4cd1
Alejandra Dean <alejandra.dean@gmail.com>
2018-07-03 15:22:00 -0400

    started adding in os.walk functions

--- test_xml_parse.py
+++ test_xml_parse.py
@@ -1,26 +1,35 @@
+# INCOMPLETE SCRIPT
 # pip install lxml
-
+import os 
+import csv
 from lxml import etree
-doc = etree.parse(operationalMetadata.xml)
 
-# <folder> dgs attribute
-folderElem = doc.find('folder')
-print(folderElem.get('dgs'))
-dgs = folderElem.get('dgs')
+filepath = input('Enter filepath: ')
 
-# <volume-designation> text
-volDesElem = doc.find('volume-designation')
-print(volDesElem.text)
-title = volDesElem.text
+for root, dirs, files in os.walk(filepath):
+    
+    # navigate to each folder 4 levels down 
+    for dir in dirs:
+        if len(os.dirpath(dir)) > 14:
+        os.chdir
+        doc = etree.parse(operationalMetadata.xml)
 
-# <record-dates> text
-recordDatesElem = doc.find('record-dates')
-print(recordDatesElem.text)
-date = recordDatesElem.text
+        # <folder> dgs attribute
+        folderElem = doc.find('folder')
+         print(folderElem.get('dgs'))
+         dgs = folderElem.get('dgs')
+
+         # <volume-designation> text
+         volDesElem = doc.find('volume-designation')
+         print(volDesElem.text)
+         title = volDesElem.text
+
+         # <record-dates> text
+         recordDatesElem = doc.find('record-dates')
+         print(recordDatesElem.text)
+         date = recordDatesElem.text
 
 vitaltuple = (title, date, dgs)
-
-import csv
 
 mytuple = ('thing1', 'thing2', 'thing3')
 
@@ -28,4 +37,3 @@ with open('vital_records_dgs_index.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(['Title'] + ['Date'] + ['dgs Code'])
     writer.writerows([mytuple])
-
