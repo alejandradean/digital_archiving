1b92c66d15f8a8204f530aee2de9452c2e64b9ad
Alejandra Dean <alejandra.dean@gmail.com>
2018-07-03 15:25:41 -0400

    fixed typo

--- test_xml_parse.py
+++ test_xml_parse.py
@@ -10,30 +10,28 @@ for root, dirs, files in os.walk(filepath):
     
     # navigate to each folder 4 levels down 
     for dir in dirs:
-        if len(os.dirpath(dir)) > 14:
+        if len(os.dirname(dir)) > 14:
         os.chdir
         doc = etree.parse(operationalMetadata.xml)
 
         # <folder> dgs attribute
         folderElem = doc.find('folder')
-         print(folderElem.get('dgs'))
-         dgs = folderElem.get('dgs')
+        print(folderElem.get('dgs'))
+        dgs = folderElem.get('dgs')
 
-         # <volume-designation> text
-         volDesElem = doc.find('volume-designation')
-         print(volDesElem.text)
-         title = volDesElem.text
+        # <volume-designation> text
+        volDesElem = doc.find('volume-designation')
+        print(volDesElem.text)
+        title = volDesElem.text
 
-         # <record-dates> text
-         recordDatesElem = doc.find('record-dates')
-         print(recordDatesElem.text)
-         date = recordDatesElem.text
+        # <record-dates> text
+        recordDatesElem = doc.find('record-dates')
+        print(recordDatesElem.text)
+        date = recordDatesElem.text
 
-vitaltuple = (title, date, dgs)
-
-mytuple = ('thing1', 'thing2', 'thing3')
+        vitaltuple = (title, date, dgs)
 
 with open('vital_records_dgs_index.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(['Title'] + ['Date'] + ['dgs Code'])
-    writer.writerows([mytuple])
+    writer.writerows([vitaltuple])
