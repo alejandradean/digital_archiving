1c4f5cd9a257e58d79774150336317929ece8d2b
Alejandra Dean <alejandra.dean@gmail.com>
2018-07-31 16:23:02 -0400

    added append to csv line
    appends rows instead of overwriting

--- test_xml_parse.py
+++ test_xml_parse.py
@@ -9,7 +9,7 @@ for root, dirs, files in os.walk(filepath):
     
     # navigate to each folder 4 levels down 
     for dir in dirs:
-        if len(os.dirname(dir)) > 14:
+        if len(dir) > 14:
         os.chdir
         tree = ET.parse('operationalMetadata.xml')
         root = tree.getroot()
@@ -22,9 +22,8 @@ for root, dirs, files in os.walk(filepath):
 
         vitaltuple = (title, date, dgs)
 
-        with open('vital_records_dgs_index.csv', 'w', newline='') as csvfile:
+        with open('vital_records_dgs_index.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(['Title'] + ['Date'] + ['dgs Code'])
             writer.writerows([vitaltuple])
-   else:
         
