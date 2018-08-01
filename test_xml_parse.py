ea807ed6b2c64b0c2490c6eee0f795ac3c1e516f
Alejandra Dean <alejandra.dean@gmail.com>
2018-07-31 21:20:43 -0400

    edits to change directories

--- test_xml_parse.py
+++ test_xml_parse.py
@@ -1,29 +1,23 @@
-# INCOMPLETE SCRIPT
-import os 
+import os
 import csv
 import xml.etree.ElementTree as ET
 
-filepath = input('Enter filepath: ')
+for root, dirs, files in os.walk('test_parse'):
+    for filename in files:
+        if filename == 'operationalMetadata.xml':
 
-for root, dirs, files in os.walk(filepath):
-    
-    # navigate to each folder 4 levels down 
-    for dir in dirs:
-        if len(dir) > 14:
-        os.chdir
-        tree = ET.parse('operationalMetadata.xml')
-        root = tree.getroot()
+            filepath = os.path.join(root, filename)
+            tree = ET.parse(filepath)
+            root = tree.getroot()
 
-        for folder in root.findall('folder'):
-            dgs = folder.get('dgs')
-            title = folder.find('volume-designation').text
-            date = folder.find('record-dates').text
-            print(dgs, title, date)
+            for folder in root.findall('folder'):
 
-        vitaltuple = (title, date, dgs)
+                dgs = folder.get('dgs')
+                title = folder.find('volume-designation').text
+                date = folder.find('record-dates').text
 
-        with open('vital_records_dgs_index.csv', 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(['Title'] + ['Date'] + ['dgs Code'])
-            writer.writerows([vitaltuple])
-        
+            vitaltuple = (title, date, dgs)
+            
+            with open(r'C:\Users\adean\Desktop\vital_records_dgs_index.csv', 'a') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerows([vitaltuple])
