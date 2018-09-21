02ad4c038b15bfb07bd067d2e85d42442e578687
Alejandra Dean <alejandra.dean@gmail.com>
2018-09-21 10:44:58 -0400

    added comment

--- run_saxon.py
+++ run_saxon.py
@@ -1,3 +1,4 @@
 import subprocess
 
-subprocess.call(['java', '-jar', 'C:\\Program Files\\saxon9he.jar', '-s:C:\\Users\\Marcela Davison\\Desktop\\converted.xml', '-xsl:C:\\Users\\Marcela Davison\\Desktop\\hs6_07_1411.xsl', '-o:C:\\Users\\Marcela Davison\\Desktop\\metadata\\output.xml'], shell=True)
+subprocess.call(['java', '-jar', 'C:\\Program Files\\saxon9he.jar', '-s:C:\\Users\\adean\\Desktop\\converted.xml', '-xsl:C:\\Users\\adean\\Desktop\\hs6_07_1411.xsl', '-o:C:\\Users\\adean\\Desktop\\metadata\\output.xml'], shell=True)
+# temp use of \\ in Windows to resolve '\' as python escape character
