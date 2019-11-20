47d33ad6157c07a9ae00d085bcd74b8db66b575b
Alejandra Dean <alejandra.dean@gmail.com>
2019-11-20 16:31:16 -0500

    remove specific dir info

--- run_saxon.py
+++ run_saxon.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.run(['java', '-jar', 'C:\\Program Files\\saxon9he.jar', '-s:C:\\Users\\adean\\Desktop\\converted.xml', '-xsl:C:\\Users\\adean\\Desktop\\hs6_07_1411.xsl', '-o:C:\\Users\\adean\\Desktop\\metadata\\output.xml'], shell=True)
+subprocess.run(['java', '-jar', '[path to saxon - use \\ in filepath]', '-s:[path to source .xml file - use \\ in filepath]', '-xsl:[path to .xslt file - use \\ in filepath]', '-o:[path to output metadata files - use \\ in filepath]'], shell=True)
 # temp use of \\ in Windows to resolve '\' as python escape character
