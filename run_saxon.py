b70b29c734720cacf681d0a9e7e78b975fbd48ea
Alejandra Dean <alejandra.dean@gmail.com>
2020-07-30 12:30:37 -0400

    Delete run_saxon.py

--- run_saxon.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-subprocess.run(['java', '-jar', '[path to saxon - use \\ in filepath]', '-s:[path to source .xml file - use \\ in filepath]', '-xsl:[path to .xslt file - use \\ in filepath]', '-o:[path to output metadata files - use \\ in filepath]'], shell=True)
-# temp use of \\ in Windows to resolve '\' as python escape character
